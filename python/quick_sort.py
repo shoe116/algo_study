2cec4ddf46046508281c4701c1ed6487d2878b89
shusuzuk <shusuzuk@yahoo-corp.jp>
2018-04-02 01:10:08 +0900

    refactor quick_sort

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -10,45 +10,37 @@ class QuickSort(BaseSort):
   @classmethod
   def quick_sort(cls, array, left, right):
     if left < right:
-      pi, pivot_val = cls.partition(array, left, right)
-      cls.quick_sort(array, left, pi - 1)
-      cls.quick_sort(array, pi + 1, right)
+      pivot_index = cls.partition(array, left, right)
+      cls.quick_sort(array, left, pivot_index - 1)
+      cls.quick_sort(array, pivot_index + 1, right)
 
   @staticmethod
   def partition(array, left, right):
     if right - left == 1:
       if array[left] > array[right]:
-        tmp = array[left]
-        array[left] = array[right]
-        array[right] = tmp
-
-      return right, array[right]
+        array[left], array[right] = array[right], array[left]
+      return left
 
     ## init
     pivot_val = array[left]
-    pivot_index = left
-    index = left
     end = right
-    while index <= right:
+    index = left
+    while index <= end:
       tmp = array[index]
       if tmp > pivot_val:
-        array[index] = array[end]
-        array[end] = tmp
+        array[index], array[end] = array[end], array[index]
         end -= 1
-        if index == end:
-          break
       else:
-        pivot_index = index
         index += 1
     # swap left to index
-    array[left] = array[pivot_index]
-    array[pivot_index] = pivot_val
+    pivot_index = max(index - 1, left)
+    array[left],  array[pivot_index] = array[pivot_index], array[left]
 
-    return pivot_index, pivot_val
+    return pivot_index
 
 
 if __name__ == "__main__":
-  input_array = [1, 6, 4, 7, 0, 2, 2]
+  input_array = [0, 1, 6, 4, 7, 0, 2, 2]
   print(input_array)
   QuickSort.sort(input_array)
   print(input_array)
